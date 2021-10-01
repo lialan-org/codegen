7d2b3d5beb81a968c31cd34ea9ee013c79bc823c
Alan Li <alan.li@me.com>
2021-10-01 17:35:41 -0400

    Quench redundant move warning in LLVM header.

--- include/codegen/codegen.hpp
+++ include/codegen/codegen.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#if defined (__GNUC__)
+#pragma GCC diagnostic ignored "-Wredundant-move"
+#elif defined (__clang__)
+#pragma GCC diagnostic ignored "-Wredundant-move"
+#endif
+
 #include "arithmetic_ops.hpp"
 #include "builtin.hpp"
 #include "compiler_context.hpp"
@@ -15,6 +21,12 @@
 #include <llvm/Support/InitLLVM.h>
 #include <llvm/Support/TargetSelect.h>
 
+#if defined (__GNUC__)
+#pragma GCC diagnostic pop
+#elif defined (__clang__)
+#pragma clang diagnostic pop
+#endif
+
 #define INIT_LLVM_TARGET()                                                                                             \
   llvm::InitializeNativeTarget();                                                                                      \
   llvm::InitializeNativeTargetAsmPrinter()
@@ -22,3 +34,4 @@
 #define INIT_LLVM_ENV(argc, argv)                                                                                      \
   llvm::InitLLVM x(argc, argv);                                                                                        \
   INIT_LLVM_TARGET()
+
